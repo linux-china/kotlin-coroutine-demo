8d88c45b8e2f3133a89b839f0cc36b2248483d5a
linux_china <libing.chen@gmail.com>
2019-04-29 09:07:37 +0800

    update artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     kotlin("jvm") version "1.3.30"
-    id("kotlinx-atomicfu").version("0.12.3")
+    id("kotlinx-atomicfu").version("0.12.5")
 }
 
 application {
@@ -17,11 +17,11 @@ dependencies {
     compile(kotlin("stdlib", "1.3.30"))
     compile(kotlin("stdlib-jdk8", "1.3.30"))
     testCompile(kotlin("test-junit5", "1.3.30"))
-    compile("org.jetbrains.kotlinx:atomicfu:0.12.3")
+    compile("org.jetbrains.kotlinx:atomicfu:0.12.5")
 
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.0")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.2.0")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.2.0")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.2.1")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.2.1")
     testCompile("org.junit.jupiter:junit-jupiter-api:5.4.2")
     testCompile("org.junit.jupiter:junit-jupiter-params:5.4.2")
 }
