cd1a3ee1a39e5f47dacfc72587b578102f7266d2
linux_china <libing.chen@gmail.com>
2019-11-20 10:45:19 +0800

    Update artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.3.50"
+val kotlinVersion = "1.3.60"
 val coroutinesVersion = "1.3.2"
 val junit5Version = "5.5.2"
 
@@ -17,7 +17,7 @@ buildscript {
 
 plugins {
     application
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.60"
     id("kotlinx-atomicfu").version("0.13.1")
 }
 
@@ -29,15 +29,15 @@ application {
 }
 
 dependencies {
-    compile("org.jetbrains:annotations:17.0.0")
+    compile("org.jetbrains:annotations:18.0.0")
     compile(kotlin("stdlib", kotlinVersion))
     compile(kotlin("stdlib-jdk8", kotlinVersion))
     compile(kotlin("reflect", kotlinVersion))
     testCompile(kotlin("test-junit5", kotlinVersion))
-    compile("org.jetbrains.kotlinx:atomicfu:0.13.1")
+    compile("org.jetbrains.kotlinx:atomicfu:0.14.1")
     compile("com.squareup.retrofit2:retrofit:2.6.0")
     compile("com.squareup.retrofit2:converter-jackson:2.6.0")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.9")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.0")
 
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutinesVersion}")
