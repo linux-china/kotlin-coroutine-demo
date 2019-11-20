3041ff9cea4b166c5f4754eeee666cc063a20daa
linux_china <libing.chen@gmail.com>
2019-11-20 10:45:01 +0800

    remove buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,5 @@
 rootProject.name = "kotlin-coroutine-demo"
 
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.11.12")
-    }
-
-    repositories {
-        jcenter()
-    }
-}
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
