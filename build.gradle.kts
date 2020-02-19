4c0721524558eeb878b984f9fd731480dd063186
linux_china <libing.chen@gmail.com>
2020-02-19 08:31:37 -0800

    Polishn

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val kotlinVersion = "1.3.61"
 val coroutinesVersion = "1.3.3"
-val junit5Version = "5.5.2"
+val junit5Version = "5.6.0"
 
 
 buildscript {
@@ -29,21 +29,21 @@ application {
 }
 
 dependencies {
-    compile("org.jetbrains:annotations:18.0.0")
-    compile(kotlin("stdlib", kotlinVersion))
-    compile(kotlin("stdlib-jdk8", kotlinVersion))
-    compile(kotlin("reflect", kotlinVersion))
-    testCompile(kotlin("test-junit5", kotlinVersion))
-    compile("org.jetbrains.kotlinx:atomicfu:0.14.1")
-    compile("com.squareup.retrofit2:retrofit:2.6.2")
-    compile("com.squareup.retrofit2:converter-jackson:2.6.2")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
+    implementation("org.jetbrains:annotations:18.0.0")
+    implementation(kotlin("stdlib", kotlinVersion))
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
+    implementation(kotlin("reflect", kotlinVersion))
+    testImplementation(kotlin("test-junit5", kotlinVersion))
+    implementation("org.jetbrains.kotlinx:atomicfu:0.14.1")
+    implementation("com.squareup.retrofit2:retrofit:2.6.2")
+    implementation("com.squareup.retrofit2:converter-jackson:2.6.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
 
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutinesVersion}")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${coroutinesVersion}")
-    testCompile("org.junit.jupiter:junit-jupiter-api:$junit5Version")
-    testCompile("org.junit.jupiter:junit-jupiter-params:$junit5Version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutinesVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${coroutinesVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junit5Version")
 }
 
 repositories {
