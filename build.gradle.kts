daf6fecb1a3f5b6807d21ebaf88d02d7c0d21cc1
linux_china <libing.chen@gmail.com>
2020-10-13 12:02:38 -0700

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.4.0"
-val coroutinesVersion = "1.3.9"
-val junit5Version = "5.6.2"
+val kotlinVersion = "1.4.10"
+val coroutinesVersion = "1.4.0-M1"
+val junit5Version = "5.7.0"
 
 
 buildscript {
@@ -17,9 +17,9 @@ buildscript {
 
 plugins {
     application
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.4.10"
     id("kotlinx-atomicfu").version("0.14.4")
-    id("com.github.ben-manes.versions").version("0.29.0")
+    id("com.github.ben-manes.versions").version("0.33.0")
 }
 
 
@@ -30,13 +30,13 @@ application {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:20.0.0")
+    implementation("org.jetbrains:annotations:20.1.0")
     implementation(kotlin("stdlib", kotlinVersion))
     implementation(kotlin("stdlib-jdk8", kotlinVersion))
     implementation(kotlin("reflect", kotlinVersion))
     testImplementation(kotlin("test-junit5", kotlinVersion))
     implementation("org.jetbrains.kotlinx:atomicfu:0.14.3")
-    implementation("io.projectreactor:reactor-core:3.3.9.RELEASE")
+    implementation("io.projectreactor:reactor-core:3.3.10.RELEASE")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.2")
