7389364deb03111d8b4982e9f9424ca8b278c0bf
linux_china <libing.chen@gmail.com>
2021-02-24 11:05:55 -0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.4.21"
+val kotlinVersion = "1.4.30"
 val coroutinesVersion = "1.4.2"
-val junit5Version = "5.7.0"
+val junit5Version = "5.7.1"
 
 
 buildscript {
     repositories {
         mavenCentral()
-        jcenter()
     }
     dependencies {
-        classpath("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.1.0")
+        classpath("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.2.0")
     }
 }
 
 plugins {
     application
-    kotlin("jvm") version "1.4.21"
-    id("kotlinx-atomicfu").version("0.15.0")
+    kotlin("jvm") version "1.4.30"
+    id("kotlinx-atomicfu").version("0.15.1")
     id("com.github.ben-manes.versions").version("0.36.0")
 }
 
@@ -26,7 +25,7 @@ plugins {
 application {
     group = "org.mvnsearch"
     version = "1.0.0-SNAPSHOT"
-    mainClassName = "demo.DemoKt"
+    mainClass.value("demo.DemoKt")
 }
 
 dependencies {
@@ -35,11 +34,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8", kotlinVersion))
     implementation(kotlin("reflect", kotlinVersion))
     testImplementation(kotlin("test-junit5", kotlinVersion))
-    implementation("org.jetbrains.kotlinx:atomicfu:0.15.0")
-    implementation("io.projectreactor:reactor-core:3.4.1")
+    implementation("org.jetbrains.kotlinx:atomicfu:0.15.1")
+    implementation("io.projectreactor:reactor-core:3.4.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutinesVersion}")
