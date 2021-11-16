ef2e75a10ebaf19ffcb13bc130686b52f24c6744
linux_china <libing.chen@gmail.com>
2021-11-16 15:02:47 -0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.5.31"
+val kotlinVersion = "1.6.0"
 val coroutinesVersion = "1.5.2"
-val junit5Version = "5.8.0"
+val junit5Version = "5.8.1"
 
 
 buildscript {
@@ -17,8 +17,8 @@ repositories {
 
 plugins {
     application
-    kotlin("jvm") version "1.5.31"
-    id("kotlinx-atomicfu").version("0.16.3")
+    kotlin("jvm") version "1.6.0"
+    id("kotlinx-atomicfu").version("0.17.0")
     id("com.github.ben-manes.versions").version("0.39.0")
 }
 
@@ -30,16 +30,16 @@ application {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:22.0.0")
+    implementation("org.jetbrains:annotations:23.0.0")
     implementation(kotlin("stdlib", kotlinVersion))
     implementation(kotlin("stdlib-jdk8", kotlinVersion))
     implementation(kotlin("reflect", kotlinVersion))
     testImplementation(kotlin("test-junit5", kotlinVersion))
-    implementation("org.jetbrains.kotlinx:atomicfu:0.16.3")
-    implementation("io.projectreactor:reactor-core:3.4.10")
+    implementation("org.jetbrains.kotlinx:atomicfu:0.17.0")
+    implementation("io.projectreactor:reactor-core:3.4.12")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutinesVersion}")
@@ -54,7 +54,7 @@ val compiler = javaToolchains.compilerFor {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "16"
+        jvmTarget = "17"
         javaParameters = true
         freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjvm-default=all")
     }
